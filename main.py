902f515cca7b3e79a3ba75ec937be7fa7fc0205e
Coent50 <149325574+Coent50@users.noreply.github.com>
2023-11-06 14:59:53 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Imports
+import argparse
+import csv
+from datetime import date
+import functions
+
+# Do not change these lines.
+__winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
+__human_name__ = "superpy"
+
+
+# Your code below this line.
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+print(functions.inventory_report())
